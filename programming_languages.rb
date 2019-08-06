51d092d13ff32eab388fc51f844a138d6b1a90ce
Pete Hanner <petehanner@gmail.com>
2019-08-05 20:28:09 -0400

    Created new hash format

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,26 @@
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+  # initialize hash
+  new_hash = {}
+
+  # loop over each language
+  languages.each do |style, langs|
+    langs.each do |lang, type|
+      # check if language exists in new_hash
+      if !(new_hash[lang])
+        # populate hash with key-value pairs in new format
+        new_hash[lang] = {
+          :type => type[:type],
+          :style => [style]
+        }
+      # if language is already there, push style onto array
+      else
+        new_hash[lang][:style] << style
+      end
+    end
+  end
+
+  # return value
+  new_hash
 end
